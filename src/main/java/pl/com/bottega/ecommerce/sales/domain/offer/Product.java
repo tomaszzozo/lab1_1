442346ed88002bdb617fdfe6f2bff7c2a42da66f
Wojtkiewicz.Tomasz <222616@edu.p.lodz.pl>
2022-03-11 19:02:20 +0100

    Added product class

---
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Product.java
@@ -0,0 +1,40 @@
+package pl.com.bottega.ecommerce.sales.domain.offer;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class Product {
+    private String productId;
+    private Money productPrice;
+    private String productName;
+    private Date productSnapshotDate;
+    private String productType;
+
+    public Product(String productId, Money productPrice, String productName, Date productSnapshotDate, String productType) {
+        this.productId = productId;
+        this.productPrice = productPrice;
+        this.productName = productName;
+        this.productSnapshotDate = productSnapshotDate;
+        this.productType = productType;
+    }
+
+    public String getProductType() {
+        return productType;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public Date getProductSnapshotDate() {
+        return productSnapshotDate;
+    }
+
+    public Money getProductPrice() {
+        return productPrice;
+    }
+}
